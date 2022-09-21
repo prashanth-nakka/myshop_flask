f5c5dba83972ddd2e5eead05c27033dc7f468dab
Tooni <prashanth04@gmail.com>
2022-09-21 09:14:30 +0530

    Render Templte Process

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -6,3 +6,8 @@ from shop import app, db
 @app.route("/")
 def home():
     return "Home Page"
+
+
+@app.route("/register")
+def register():
+    return render_template("admin/register.html", title="Register")
